caa3066559583e2b5f1f4bf5380f0bd4ed0cec26
yesandv <grc@protonmail.com>
2024-09-13 23:02:17 +0300

    Add views

--- docker_compose/movies/app/movies/api/v1/urls.py
+++ docker_compose/movies/app/movies/api/v1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from movies.api.v1 import views
+from ...api.v1 import views
 
 urlpatterns = [
-    path('movies/', views.api),
+    path('movies/', views.FilmWorkListApi.as_view()),
+    path("movies/<uuid:pk>/", views.FilmWorkDetailApi.as_view())
 ]
--- docker_compose/movies/app/movies/api/v1/views.py
+++ docker_compose/movies/app/movies/api/v1/views.py
@@ -1,5 +1,86 @@
-from django.http import HttpResponse
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models import Q, QuerySet, Value
+from django.db.models.functions import Coalesce
+from django.http import JsonResponse
+from django.views.generic.detail import BaseDetailView
+from django.views.generic.list import BaseListView
+
+from ...models import FilmWork, Roles
 
 
-def api(request):
-    return HttpResponse("My best API")
+class FilmWorkApiMixin:
+    model = FilmWork
+    http_method_names = ["get"]
+
+    @staticmethod
+    def _annotate(obj: QuerySet) -> QuerySet:
+        return obj.annotate(
+            genres=Coalesce(
+                ArrayAgg("genres__name", distinct=True), Value([])
+            ),
+            actors=Coalesce(
+                ArrayAgg(
+                    "persons__full_name",
+                    filter=Q(personfilmwork__role=Roles.ACTOR),
+                    distinct=True,
+                ),
+                Value([]),
+            ),
+            directors=Coalesce(
+                ArrayAgg(
+                    "persons__full_name",
+                    filter=Q(personfilmwork__role=Roles.DIRECTOR),
+                    distinct=True,
+                ),
+                Value([]),
+            ),
+            writers=Coalesce(
+                ArrayAgg(
+                    "persons__full_name",
+                    filter=Q(personfilmwork__role=Roles.WRITER),
+                    distinct=True,
+                ),
+                Value([]),
+            ),
+        )
+
+    def get_queryset(self) -> QuerySet:
+        film_works = self.model.objects.prefetch_related(  # noqa
+            "genres", "persons"
+        ).all()
+        film_work_fields = film_works.values(
+            "id", "title", "description", "creation_date", "rating", "type"
+        )
+        annotated_film_works = self._annotate(film_work_fields)
+        return annotated_film_works
+
+    def render_to_response(self, context, **response_kwargs):  # noqa
+        return JsonResponse(context)
+
+
+class FilmWorkListApi(FilmWorkApiMixin, BaseListView):
+    paginate_by = 50
+
+    def get_context_data(
+            self, *, object_list: QuerySet = None, **kwargs
+    ) -> dict:
+        queryset = self.get_queryset()
+        paginator, page, queryset, is_paginated = self.paginate_queryset(
+            queryset, self.paginate_by
+        )
+        context = {
+            "count": paginator.count,
+            "total_pages": paginator.num_pages,
+            "prev": (
+                page.previous_page_number() if page.has_previous() else None
+            ),
+            "next": page.next_page_number() if page.has_next() else None,
+            "results": list(queryset),
+        }
+        return context
+
+
+class FilmWorkDetailApi(FilmWorkApiMixin, BaseDetailView):
+
+    def get_context_data(self, **kwargs) -> dict:
+        return self.get_object()
