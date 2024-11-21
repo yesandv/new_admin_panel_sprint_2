7b3d8a1bf23a2738c338bc5d745557f4519cc25a
yesandv <grc@protonmail.com>
2024-11-21 15:44:23 +0200

    Update settings

--- docker_compose/movies/app/config/settings.py
+++ docker_compose/movies/app/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False) == "True"
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]", "admin"]
 
 # Application definition
 
